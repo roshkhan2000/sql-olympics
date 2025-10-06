2026b3a4f564af118cb5ca797beadc05cb3ccfc5
Rosh Khan <rosh.khan+b2s@theinformationlab.co.uk>
2025-10-06 18:22:25 +0000

    start-of-gold-layer

---
+++ models/olympics/gold/running_gold_medalists.sql
@@ -0,0 +1,32 @@
+SELECT
+    r.result_id,
+    r.athlete_id,
+    s.sex AS sex
+    a.name AS athelete_name,
+    r.event_id,
+    e.event AS event,
+    g.year AS year,
+    g.city AS city,
+    g.games AS game,
+    g.sport AS sport,
+    r.medal
+FROM 
+    {{ ref('int_results') }} AS r
+LEFT JOIN 
+    {{ ref('int_athletes') }} AS a
+ON 
+    r.athlete_id = a.athlete_id
+LEFT JOIN
+    {{ ref('int_athletes_stats') }} AS s
+ON
+    s.athlete_id = r.athlete_id
+LEFT JOIN
+    {{ ref('int_events') }} AS e
+ON
+    r.event_id = e.event_id
+LEFT JOIN
+    {{ ref('int_games') }} AS g
+ON
+    g.event_id = e.event_id
+WHERE
+    medal = 'Gold'
